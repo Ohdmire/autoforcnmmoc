7ea75f8ff24d6618343210c3042b7b4886a8cd58
Ohdmire <1750011571@qq.com>
2023-11-24 17:00:47 +0800

    Update exam_main.js

--- exam_main.js
+++ exam_main.js
@@ -9,8 +9,7 @@
 // @match        http://180.76.151.202/study/*
 // @match        https://www.cnmooc.org/study/*
 // @grant        none
-// @connect      raw.githubusercontent.com
-// @require      https://raw.githubusercontent.com/wendux/ajax-hook/master/dist/ajaxhook.min.js
+// @require      https://unpkg.com/ajax-hook/dist/ajaxhook.min.js
 // @license      MIT
 
 // ==/UserScript==
